91add99231e079a0118dfe6e5f53684e5e81e6d1
JasperTresidder <51917264+JasperTresidder@users.noreply.github.com>
2019-09-02 19:06:24 +0100

    Update darts.js

--- Darts/darts.js
+++ Darts/darts.js
@@ -1,4 +1,4 @@
-
+let scaleval = 1.3;
 
 i = 0;
 j = 0;
@@ -12,10 +12,16 @@ legTurn = true;
 
 P1allShots = [];
 P2allShots = [];
+P1LegS = [];
+P2LegS = [];
 
 P1Curr = 501;
 P2Curr = 501;
 
+P1LastLeg = 0;
+P2LastLeg = 0;
+
+let fin;
 var P1Score = 0;
 var P2Score = 0;
 
@@ -23,17 +29,20 @@ let score = 0;
 let throws = 0;
 
 let r = 150;
-let numb = [20,1,18,4,13,6,10,15,2,17,3,19,7,16,8,11,14,9,12,5];
+let numb = [20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5];
 
 const w = 500;
 const h = 600;
 
 let P1;
 let P2;
-let scaleval;
+
+let P1LegAv = 0;
+let P2LegAv = 0;
+
 
 function setup() {
-  scaleval = displayWidth/1000;
+
   P1 = 'Jasper';
   P2 = 'Titus';
   P1av = float(0.0);
@@ -47,34 +56,34 @@ function setup() {
   updateScore();
   textAlign(CENTER);
   input = createInput();
-  input.position((w / 2 - 6)*scaleval, (h / 40)*scaleval);
-  input.size(15);
+  input.position((w / 2 - 66) * scaleval, (h / 40) * scaleval);
   button = createButton('Undo');
-  button.position((9 * w / 10)*scaleval, scaleval*5 * h / 80);
+  button.position((9 * w / 10) * scaleval, scaleval * h / 40);
   button.mousePressed(Undo);
 
-  button1 = createButton('Turn');
-  button1.position((w / 2 - 26 + w / 32)*scaleval, scaleval*5 * h / 80);
+  button1 = createButton('Change Turn');
+  button1.position((w / 2 - 59 + w / 32) * scaleval, scaleval * 5 * h / 80);
   button1.mousePressed(changePlayer);
-  
-  
+
+
   buttonx = createButton('Reset');
-  buttonx.position((500+(r)*2)*scaleval, scaleval*19);
+  buttonx.position((500 + (r) * 2) * scaleval, scaleval * 19);
   buttonx.mousePressed(ResetScore);
-  
+
   buttony = createButton('Merge');
-  buttony.position(scaleval*680,scaleval*420);
+  buttony.position(scaleval * 680, scaleval * 420);
   buttony.mousePressed(Merge);
-  
-  
+
+
   DrawBoard();
   DrawNumbers();
-
-
+  for (var i = 0; i < fin.length; i++) {
+    console.log(fin[i][0], fin[i][1]);
+  }
 }
 
 
-function ResetScore(){
+function ResetScore() {
   score = 0;
   throws = 0;
   DrawBoard();
@@ -82,193 +91,193 @@ function ResetScore(){
   //console.log('ok');
 }
 
-function DrawBoard(){
+function DrawBoard() {
   stroke(0);
   strokeWeight(1);
   push();
-  translate(700,200);
-  r = r/2;
+  translate(700, 200);
+  r = r / 2;
   angleMode(DEGREES);
   fill(51);
   //outerboard
   //console.log(r);
-  circle(0,0,(r+r/4)*2);
-  fill(255,0,0);
+  circle(0, 0, (r + r / 4) * 2);
+  fill(255, 0, 0);
   //doubles
-  circle(0,0,r*2);
-  fill(255,255,210);
-  circle(0,0,r*2 - r/10);
-  fill(0,255,0);
+  circle(0, 0, r * 2);
+  fill(255, 255, 210);
+  circle(0, 0, r * 2 - r / 10);
+  fill(0, 255, 0);
   //triples
-  circle(0,0,r + r/5);
-  fill(255,255,210);
-  circle(0,0,r + r/10);
-  fill(0,255,0);
+  circle(0, 0, r + r / 5);
+  fill(255, 255, 210);
+  circle(0, 0, r + r / 10);
+  fill(0, 255, 0);
   //middle
-  circle(0,0,r/5);
-  fill(255,0,0);
-  circle(0,0,r/10);
-  let a = 360/40;
-  for(let i = 0; i < 20 ; i++){
-    let x = 2*r*cos(a);
-    let y = 2*r*sin(a);
-    line(r*cos(a)/5,r*sin(a)/5,x,y);
-    a += 360/20; 
+  circle(0, 0, r / 5);
+  fill(255, 0, 0);
+  circle(0, 0, r / 10);
+  let a = 360 / 40;
+  for (let i = 0; i < 20; i++) {
+    let x = 2 * r * cos(a);
+    let y = 2 * r * sin(a);
+    line(r * cos(a) / 5, r * sin(a) / 5, x, y);
+    a += 360 / 20;
   }
   pop();
   strokeWeight(0);
   noStroke();
 }
 
-function DrawNumbers(){
+function DrawNumbers() {
   push();
-  r = r*2;
-  translate(700,200);
+  r = r * 2;
+  translate(700, 200);
   textSize(16);
   fill(255);
   let a = 18;
   //
-  for(let i = 0; i < 20 ; i++){
-    if(i>5 && i<15){
+  for (let i = 0; i < 20; i++) {
+    if (i > 5 && i < 15) {
       push();
-      translate(0,-r - 20);
+      translate(0, -r - 20);
       rotate(180);
-      text(str(numb[i]),0,0);
+      text(str(numb[i]), 0, 0);
       pop();
-    }else{
-      text(str(numb[i]),0,-r - 10);
+    } else {
+      text(str(numb[i]), 0, -r - 10);
     }
     rotate(a);
   }
   //
   textSize(30);
   fill(0);
-  text(score,-160,-160);
+  text(score, -160, -160);
   pop();
 }
 
-function mouseClicked(){
-  if(throws == 3){
+function mouseClicked() {
+  if (throws == 3) {
     score = 0;
     throws = 0;
   }
-  
+
   //console.log(mouseX-200,mouseY-200);
-  let x = mouseX-(700*scaleval);
-  let y = mouseY-(200*scaleval);
-  let a = atan(y/x);
-  let rad = dist((700*scaleval),(200*scaleval),mouseX,mouseY);
+  let x = mouseX - (700 * scaleval);
+  let y = mouseY - (200 * scaleval);
+  let a = atan(y / x);
+  let rad = dist((700 * scaleval), (200 * scaleval), mouseX, mouseY);
   //console.log(abs(a));
   let index = 0;
-  
+
   //Find Index
-  
+
   a = abs(a);
-  
+
   //Top Left
-  if(x<=0 && y<=0){
-    if(a < 9){
+  if (x <= 0 && y <= 0) {
+    if (a < 9) {
       index = 15;
     }
-    if(a>9 && a<27){
+    if (a > 9 && a < 27) {
       index = 16;
     }
-    if(a>27 && a<45){
+    if (a > 27 && a < 45) {
       index = 17;
     }
-    if(a>45 && a<63){
+    if (a > 45 && a < 63) {
       index = 18;
     }
-    if(a>63 && a<81){
+    if (a > 63 && a < 81) {
       index = 19;
     }
-    if(a > 81){
+    if (a > 81) {
       index = 0;
     }
   }
-  
-  if(x>=0 && y<=0){
-    if(a < 9){
+
+  if (x >= 0 && y <= 0) {
+    if (a < 9) {
       index = 5;
     }
-    if(a>9 && a<27){
+    if (a > 9 && a < 27) {
       index = 4;
     }
-    if(a>27 && a<45){
+    if (a > 27 && a < 45) {
       index = 3;
     }
-    if(a>45 && a<63){
+    if (a > 45 && a < 63) {
       index = 2;
     }
-    if(a>63 && a<81){
+    if (a > 63 && a < 81) {
       index = 1;
     }
-    if(a > 81){
+    if (a > 81) {
       index = 0;
     }
   }
-  if(x>=0 && y>=0){
-    if(a < 9){
+  if (x >= 0 && y >= 0) {
+    if (a < 9) {
       index = 5;
     }
-    if(a>9 && a<27){
+    if (a > 9 && a < 27) {
       index = 6;
     }
-    if(a>27 && a<45){
+    if (a > 27 && a < 45) {
       index = 7;
     }
-    if(a>45 && a<63){
+    if (a > 45 && a < 63) {
       index = 8;
     }
-    if(a>63 && a<81){
+    if (a > 63 && a < 81) {
       index = 9;
     }
-    if(a > 81){
+    if (a > 81) {
       index = 10;
     }
   }
-  
-  if(x<=0 && y>=0){
-    if(a < 9){
+
+  if (x <= 0 && y >= 0) {
+    if (a < 9) {
       index = 15;
     }
-    if(a>9 && a<27){
+    if (a > 9 && a < 27) {
       index = 14;
     }
-    if(a>27 && a<45){
+    if (a > 27 && a < 45) {
       index = 13;
     }
-    if(a>45 && a<63){
+    if (a > 45 && a < 63) {
       index = 12;
     }
-    if(a>63 && a<81){
+    if (a > 63 && a < 81) {
       index = 11;
     }
-    if(a > 81){
+    if (a > 81) {
       index = 10;
     }
   }
-  r = 150*scaleval;
-  
+  r = 150 * scaleval;
+
   //console.log(index);
-  if(rad < (r + r/5)/2 && rad > (r + r/10)/2){
-    score += 3*numb[index];
-    throws ++;
-  }else if(rad < (r*2)/2 && rad > (r*2 - r/10)/2){
-    score += 2*numb[index];
-    throws ++;
-  }else if(rad < (r*2 - r/10)/2 && rad > (r/5)/2){
+  if (rad < (r + r / 5) / 2 && rad > (r + r / 10) / 2) {
+    score += 3 * numb[index];
+    throws++;
+  } else if (rad < (r * 2) / 2 && rad > (r * 2 - r / 10) / 2) {
+    score += 2 * numb[index];
+    throws++;
+  } else if (rad < (r * 2 - r / 10) / 2 && rad > (r / 5) / 2) {
     score += numb[index];
-    throws ++;
-  }else if(rad < (r/5)/2 && rad > (r/10)/2){
+    throws++;
+  } else if (rad < (r / 5) / 2 && rad > (r / 10) / 2) {
     score += 25;
-    throws ++;
-  }else if(rad < (r/10)/2){
+    throws++;
+  } else if (rad < (r / 10) / 2) {
     score += 50;
-    throws ++;
-  }else if(rad < (r+r/4)){
+    throws++;
+  } else if (rad < (r + r / 4)) {
     score += 0;
-    throws ++;
+    throws++;
   }
   r = 150;
   drawGrid();
@@ -287,7 +296,7 @@ function draw() {
   } else {
     ellipse(3 * w / 4, h / 6, 7, 7, 0);
   }
-  if(P1Curr < 0 || P2Curr < 0 ){
+  if (P1Curr < 0 || P2Curr < 0) {
     Undo();
   }
 }
@@ -314,7 +323,7 @@ function drawGrid() {
 
 }
 
-function Merge(){
+function Merge() {
   drawGrid();
   DrawBoard();
   DrawNumbers();
@@ -328,10 +337,12 @@ function Merge(){
     P1Curr = P1Curr - score;
     P1Leg.push(P1Curr);
     P1allShots.push(score);
+    P1LegS.push(score);
   } else {
     P2Curr = P2Curr - score;
     P2Leg.push(P2Curr);
     P2allShots.push(score);
+    P2LegS.push(score);
   }
   drawScore();
   Turn = !Turn;
@@ -362,6 +373,22 @@ function Merge(){
 
   P2av = round(100 * P2av);
   P2av = P2av / 100;
+
+  temp = 0;
+  for (k = 0; k < P2LegS.length; k++) {
+    temp = temp + int(P2LegS[k]);
+  }
+  P2LegAv = float(temp / P2LegS.length);
+  P2LegAv = round(100 * P2LegAv);
+  P2LegAv = P2LegAv / 100;
+
+  temp = 0;
+  for (k = 0; k < P1LegS.length; k++) {
+    temp = temp + int(P1LegS[k]);
+  }
+  P1LegAv = float(temp / P1LegS.length);
+  P1LegAv = round(100 * P1LegAv);
+  P1LegAv = P1LegAv / 100;
   updateScore();
   ResetScore();
 }
@@ -375,20 +402,44 @@ function updateScore() {
   fill(10, 0, 145);
   if (lastWin == true) {
     text(str(P2av), w / 8 + w / 2 - w / 32, 3 * h / 20);
+    text('P2 Leg AVR: ' + P2LegAv, 2 * width / 4, 2 * height / 4 + 80);
   } else if (lastWin == false) {
     text(str(P1av), w / 8 - w / 32, 3 * h / 20);
+    text('P1 Leg AVR: ' + P1LegAv, 2 * width / 4, 2 * height / 4 + 50);
   } else {
     if (isNaN(P1av) == true) {
       text('0', w / 8 - w / 32, 3 * h / 20);
+      if (isNaN(P1LegAv) == true) {
+        text('P1 Leg AVR: ' + '0', 2 * width / 4, 2 * height / 4 + 50);
+      }else{
+        text('P1 Leg AVR: ' + P1LegAv, 2 * width / 4, 2 * height / 4 + 50);
+      }
     } else {
       text(str(P1av), w / 8 - w / 32, 3 * h / 20);
+      if (isNaN(P1LegAv) == true) {
+        text('P1 Leg AVR: ' + '0', 2 * width / 4, 2 * height / 4 + 50);
+      }else{
+        text('P1 Leg AVR: ' + P1LegAv, 2 * width / 4, 2 * height / 4 + 50);
+      }
     }
     if (isNaN(P2av) == true) {
       text('0', w / 8 + w / 2 - w / 32, 3 * h / 20);
+      if (isNaN(P2LegAv) == true) {
+        text('P2 Leg AVR: ' + '0', 2 * width / 4, 2 * height / 4 + 80);
+      }else{
+        text('P2 Leg AVR: ' + P2LegAv, 2 * width / 4, 2 * height / 4 + 80);
+      }
     } else {
       text(str(P2av), w / 8 + w / 2 - w / 32, 3 * h / 20);
+      if (isNaN(P2LegAv) == true) {
+        text('P2 Leg AVR: ' + '0', 2 * width / 4, 2 * height / 4 + 80);
+      }else{
+        text('P2 Leg AVR: ' + P2LegAv, 2 * width / 4, 2 * height / 4 + 80);
+      }
     }
   }
+  text('P1 last Leg AVR: '+ P1LastLeg, 2 * width / 4, 2 * height / 4 + 130);
+  text('P2 last Leg AVR: '+P2LastLeg, 2 * width / 4, 2 * height / 4 + 160);
   lastWin = null;
   fill(0);
 }
@@ -409,10 +460,12 @@ function Score() {
     P1Curr = P1Curr - value;
     P1Leg.push(P1Curr);
     P1allShots.push(value);
+    P1LegS.push(value);
   } else {
     P2Curr = P2Curr - value;
     P2Leg.push(P2Curr);
     P2allShots.push(value);
+    P2LegS.push(value);
   }
   drawScore();
   //DrawBoard();
@@ -445,6 +498,23 @@ function Score() {
 
   P2av = round(100 * P2av);
   P2av = P2av / 100;
+
+  temp = 0;
+  for (k = 0; k < P2LegS.length; k++) {
+    temp = temp + int(P2LegS[k]);
+  }
+  P2LegAv = float(temp / P2LegS.length);
+  P2LegAv = round(100 * P2LegAv);
+  P2LegAv = P2LegAv / 100;
+
+  temp = 0;
+  for (k = 0; k < P1LegS.length; k++) {
+    temp = temp + int(P1LegS[k]);
+  }
+  P1LegAv = float(temp / P1LegS.length);
+  P1LegAv = round(100 * P1LegAv);
+  P1LegAv = P1LegAv / 100;
+
   updateScore();
 }
 
@@ -455,8 +525,18 @@ function redo() {
   Turn = legTurn;
   P1Leg = [];
   P2Leg = [];
+  P1LegS = [];
+  P2LegS = [];
   P1Curr = 501;
   P2Curr = 501;
+  drawGrid();
+  updateScore();
+  DrawBoard();
+  DrawNumbers();
+  P1LastLeg = P1LegAv;
+  P2LastLeg = P2LegAv;
+  P1LegAv = 0;
+  P2LegAv = 0;
   drawGrid();
   updateScore();
   DrawBoard();
@@ -464,7 +544,7 @@ function redo() {
 }
 
 function keyTyped() {
-  if ((keyCode == ENTER || key == ' ')& int(input.value()) < 181 & int(input.value()) > -1 & isNaN(input.value()) == false) {
+  if ((keyCode == ENTER || key == ' ') & int(input.value()) < 181 & int(input.value()) > -1 & isNaN(input.value()) == false) {
     if (Turn == true & int(input.value()) < P1Curr + 1) {
       Score();
     }
@@ -483,6 +563,7 @@ function Undo() {
         P2Curr = P2Leg[P2Leg.length - 2];
       }
       P2allShots.pop();
+      P2LegS.pop();
       P2Leg.pop();
       Turn = !Turn;
       drawGrid();
@@ -509,6 +590,23 @@ function Undo() {
 
       P2av = round(100 * P2av);
       P2av = P2av / 100;
+
+      temp = 0;
+      for (k = 0; k < P2LegS.length; k++) {
+        temp = temp + int(P2LegS[k]);
+      }
+      P2LegAv = float(temp / P2LegS.length);
+      P2LegAv = round(100 * P2LegAv);
+      P2LegAv = P2LegAv / 100;
+
+      temp = 0;
+      for (k = 0; k < P1LegS.length; k++) {
+        temp = temp + int(P1LegS[k]);
+      }
+      P1LegAv = float(temp / P1LegS.length);
+      P1LegAv = round(100 * P1LegAv);
+      P1LegAv = P1LegAv / 100;
+
       updateScore();
     }
   } else {
@@ -520,6 +618,7 @@ function Undo() {
       }
       P1allShots.pop();
       P1Leg.pop();
+      P1LegS.pop();
       Turn = !Turn;
       drawGrid();
       fill(0);
@@ -544,6 +643,22 @@ function Undo() {
 
       P2av = round(100 * P2av);
       P2av = P2av / 100;
+
+      temp = 0;
+      for (k = 0; k < P2Leg.length; k++) {
+        temp = temp + int(P2Leg[k]);
+      }
+      P2LegAv = float(temp / P2Leg.length);
+      P2LegAv = round(100 * P2av);
+      P2LegAv = P2LegAv / 100;
+
+      temp = 0;
+      for (k = 0; k < P1Leg.length; k++) {
+        temp = temp + int(P1Leg[k]);
+      }
+      P1LegAv = float(temp / P1Leg.length);
+      P1LegAv = round(100 * P1av);
+      P1LegAv = P1LegAv / 100;
       updateScore();
     }
   }
@@ -556,6 +671,9 @@ function changePlayer() {
   drawScore();
   DrawBoard();
   DrawNumbers();
+  if (P1Leg.length == 0 && P2Leg.length == 0) {
+    legTurn = !legTurn;
+  }
 }
 
 function drawScore() {
@@ -566,11 +684,31 @@ function drawScore() {
       text(str(P1Leg[i]), w / 4 + w / 8, 6 * h / 20 + (i - 15) * h / 20);
     }
   }
+  for (var i = 0; i < fin.length; i++) {
+    if (P1Leg[P1Leg.length - 1] == int(fin[i][0])) {
+      push();
+      stroke(0, 255, 0);
+      textAlign(CENTER);
+      textSize(12);
+      text(str(fin[i][1]), w / 4, height / 16);
+      pop();
+    }
+  }
   for (var j = 0; j < P2Leg.length; j++) {
     if (j < 14) {
       text(str(P2Leg[j]), 3 * w / 4 - w / 8, 6 * h / 20 + j * h / 20);
     } else {
       text(str(P2Leg[j]), 3 * w / 4 + w / 8, 6 * h / 20 + (j - 15) * h / 20);
+    }
+  }
+  for (var i = 0; i < fin.length; i++) {
+    if (P2Leg[P2Leg.length - 1] == int(fin[i][0])) {
+      push();
+      stroke(0, 255, 0);
+      textAlign(CENTER);
+      textSize(12);
+      text(str(fin[i][1]), 3 * w / 4, height / 16);
+      pop();
     }
   }
 
